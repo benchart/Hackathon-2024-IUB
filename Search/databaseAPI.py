d3321f735d396257388a625ed7900d9fce9e3585
hjac201 <huntjac@iu.edu>
2024-11-16 15:54:30 -0500

    searchDB

--- Search/databaseAPI.py
+++ Search/databaseAPI.py
@@ -6,16 +6,29 @@ from searchRepo import repoVarList , searchRepo
 import urllib
 
 app = Flask(__name__)
+query = ''
 
 
+# Initialize the connection to the SQL server
 username = 'huntjac'
 password = 'Nn39khnr!'
 server = 'hiccup-hackathon-24.database.windows.net'
 database = 'hiccup-hackathon'
 driver = 'ODBC Driver 18 for SQL Server'
 
+#Create the engine object
 connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={urllib.parse.quote_plus(driver)}&Encrypt=yes&TrustServerCertificate=no&Connection Timeout=30'
 contactsDB = create_engine(connection_string)
+
+#Store the userIDs returned by the search function and return as a combined set
+def searchDB(query):
+    contactsResults = searchContacts(query)
+    productsResults = searchProducts(query)
+    repoResults = searchRepo(query)
+    combinedUserID = set(contactsResults) | set(productsResults) | set(repoResults)
+
+    return combinedUserID
+
 
 
 # with engine.connect() as connection:
