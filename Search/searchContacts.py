d696406c0924cbe01270507a617c9b1348361e42
hjac201 <huntjac@iu.edu>
2024-11-16 16:32:35 -0500

    Wildcard update

--- Search/searchContacts.py
+++ Search/searchContacts.py
@@ -1,4 +1,5 @@
 from Search.databaseAPI import contactsDB
+from sqlalchemy import text
 
 contactTable = 'Contacts'
 firstNameCol = 'first_name'
@@ -13,17 +14,21 @@ query = ''
 #Store the variables in an Array to be passed to the API file
 contactsVarList = [contactTable, firstNameCol, lastNameCol, emailCol, usernameCol, locationCol, positionCol, userIDCol]
 
-searchString = f'"SELECT "' + userIDCol + ' FROM ' + contactTable + ' WHERE ' 
-+ firstNameCol + ' LIKE ? OR '
-+ lastNameCol + ' LIKE ? OR '
-+ emailCol + ' LIKE ? OR '
-+ usernameCol + ' LIKE ? OR '
-+ locationCol + ' LIKE ? OR '
-+ positionCol + ' LIKE ?;'
+searchString = f"""
+        SELECT {userIDCol}
+        FROM {contactTable}
+        WHERE {firstNameCol} LIKE :query 
+        OR {lastNameCol} LIKE :query
+        OR {emailCol} LIKE :query
+        OR {usernameCol} LIKE :query
+        OR {locationCol} LIKE :query
+        OR {positionCol} LIKE :query;
+"""
 
 def searchContacts(query):
-    query = str('%' + query.lower() + '%')
+    query = f"%{query.lower()}%"
     escaped_query = query.replace('%', '\\%').replace('_', '\\_')
-    result = contactsDB.execute(searchString, (escaped_query, escaped_query, escaped_query, escaped_query, escaped_query, escaped_query))
+    with contactsDB.connect() as connection:
+        result = connection.execute(text(searchString), {'query': escaped_query})
     return result.fetchAll()
 
--- Search/searchRepo.py
+++ Search/searchRepo.py
@@ -1,4 +1,5 @@
 from Search.databaseAPI import contactsDB
+from sqlalchemy import text
 
 repoTable = 'Repo'
 repoNameCol = 'repo_name'
@@ -9,12 +10,20 @@ query = ''
 #Store the variables in an Array to be passed to the API file
 repoVarList = [repoTable, repoNameCol, userIDCol, repoIDCol]
 
+searchString = f"""
+        SELECT {userIDCol}
+        FROM {repoTable}
+        WHERE {repoNameCol} LIKE :query 
+        OR {repoIDCol} LIKE :query;
+"""
+
 searchString = f'"SELECT "' + userIDCol + ' FROM ' + repoTable + ' WHERE ' 
 + repoNameCol + ' LIKE ? OR '
 + repoIDCol + ' LIKE ?;'
 
 def searchRepo(query):
-    query = str('%' + query.lower() + '%')
+    query = f"%{query.lower()}%"
     escaped_query = query.replace('%', '\\%').replace('_', '\\_')
-    result = contactsDB.execute(searchString, (escaped_query, escaped_query))
+    with contactsDB.connect() as connection:
+        result = connection.execute(text(searchString), {'query': escaped_query})
     return result.fetchAll()
