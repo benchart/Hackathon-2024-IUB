6beeb9c6c8cff4d8640f9346234c0a22f25859e0
Shashank Upadhyay <shashankupadhyay2024@gmail.com>
2024-11-17 00:22:56 -0500

    html, app updates

--- myflaskapp/app.py
+++ myflaskapp/app.py
@@ -1,23 +1,43 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, jsonify
+from databaseAPI import addEntry  # Import the addEntry function from databaseAPI.py
+from sqlalchemy import create_engine, text
+import json
 
 app = Flask(__name__)
 
-# Route to display the search form
-@app.route('/')
-def index():
-    return render_template_string(open('home.html').read())  # Serve the HTML form
+@app.route('/add-contact', methods=['POST'])
+def add_contact():
+    try:
+        # Get the JSON data from the POST request
+        data = request.get_json()
 
-# Route to handle the search logic
-@app.route('/search', methods=['GET'])
-def search():
-    # Get the 'query' parameter from the URL (search input)
-    query = request.args.get('query')
-   
-    if query:
-        # Here, you can process the 'query' variable as needed
-        return f'You searched for: {query}'  # Return the search query back to the user
-    else:
-        return 'No search query provided.'
+        # Prepare the data to be passed to the addEntry function
+        entryFieldsList = [
+            data['firstName'],
+            data['lastName'],
+            data['email'],
+            data['username'],
+            data['location'],
+            data['position'],
+            data['productName'],
+            data['repositoryName']
+        ]
 
-if __name__ == '__main__':
+        # Call the addEntry function to add the contact to the database
+        addEntry(entryFieldsList)
+
+        return jsonify({
+            "status": "success",
+            "message": "Contact added successfully!",
+            "firstName": data['firstName'],
+            "lastName": data['lastName']
+        })
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": f"Failed to add contact. Error: {str(e)}"
+        }), 400
+
+if __name__ == "__main__":
     app.run(debug=True)
